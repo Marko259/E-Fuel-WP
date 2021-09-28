a50773f0278498bc72598389816b8f38c87a2158
Marko259 <animationsisnice@gmail.com>
2021-09-28 11:00:02 +0200

    Fixed last slide img

--- wp-content/themes/efuel/front-page.php
+++ wp-content/themes/efuel/front-page.php
@@ -13,10 +13,10 @@
               <div style="height: 50px; width: 50px; background-color: #FFFF33"> <h1>Køb dansk G-Fuel her!</h1> <p>Køb G-Fuel nu her i Danmark uden told og med kort fragtid</p> </div>
             </div>
             <div class="carousel-item">
-              <img class="d-block w-100" src="C:\Users\Hp\Documents\GitHub\E-Fuel-WP\wp-content\themes\efuel\assets\images\Billede2.jpg" alt="Second slide">
+              <img class="d-block w-100" src="<?php echo get_template_directory_uri() . '\assets\images\Billede2.jpg' ?>" alt="Second slide">
             </div>
             <div class="carousel-item">
-              <img class="d-block w-100" src="C:\Users\Hp\Documents\GitHub\E-Fuel-WP\wp-content\themes\efuel\assets\images\Billede3.jpg" alt="Third slide">
+              <img class="d-block w-100" src="<?php echo get_template_directory_uri() . '\assets\images\Billede3.jpg' ?>" alt="Third slide">
             </div>
           </div>
           <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
